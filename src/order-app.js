da9f9bc5fe2dfd5f2c4e9ad69dbb0fa722ef34b1
Matt <Matt_today@163.com>
2019-07-02 19:43:50 +0800

    客户支付时打折券使用

--- src/order-app.js
+++ src/order-app.js
@@ -1,26 +1,128 @@
 import OrderRepresentation from './output/order-representation';
+var fs = require('fs');
 export default class OrderApp {
-
   checkout(orderCommand) {
     // TODO: 请完成需求指定的功能
     //1.根据客户购买的贵金属计算金额
     let orderCommandJSON = JSON.parse(orderCommand);
+
+    // 优惠政策 PreferentialPolicy 0.无优惠  1.折扣 2.第N件半价 3.满送 4.满减
+
     const items = [
-      {productNo: '001001', productName: '世园会五十国钱币册', price: 998.00 },
-      {productNo: '001002', productName: '2019北京世园会纪念银章大全40g', price: 1380.00 },
-      {productNo: '003001', productName: '招财进宝', price: 1580.00 },
-      {productNo: '003002', productName: '水晶之恋', price: 980.00 },
-      {productNo: '002002', productName: '中国经典钱币套装', price: 998.00 },
-      {productNo: '002001', productName: '守扩之羽比翼双飞4.8g', price: 1080.00 },
-      {productNo: '002003', productName: '中国银象棋12g', price: 698.00},
+      {
+        productNo: '001001',
+        productName: '世园会五十国钱币册',
+        price: 998.00,
+        PreferentialPolicy: []
+      },
+      {
+        productNo: '001002',
+        productName: '2019北京世园会纪念银章大全40g',
+        price: 1380.00,
+        PreferentialPolicy: [
+          {
+            type: 1,
+            discount: 0.9
+          }
+        ]
+      },
+      {
+        productNo: '003001',
+        productName: '招财进宝',
+        price: 1580.00,
+        PreferentialPolicy: [
+          {
+            type: 1,
+            discount: 0.95
+          }
+        ]
+      },
+      {
+        productNo: '003002',
+        productName: '水晶之恋',
+        price: 980.00,
+        PreferentialPolicy: [
+          {
+            type: 2,
+            halfPriceNum: 3
+          },
+          {
+            type: 3,
+            presentNum: 3
+          }
+        ]
+      },
+      {
+        productNo: '002002',
+        productName: '中国经典钱币套装',
+        price: 998.00,
+        PreferentialPolicy: [
+          {
+            type: 4,
+            minPrice: 2000,
+            cutDown: 30
+          },
+          {
+            type: 4,
+            minPrice: 1000,
+            cutDown: 10
+          }
+        ]
+      },
+      {
+        productNo: '002001',
+        productName: '守扩之羽比翼双飞4.8g',
+        price: 1080.00,
+        PreferentialPolicy: [
+          {
+            type: 1,
+            discount: 0.95
+          },
+          {
+            type: 2,
+            halfPriceNum: 3
+          },
+          {
+            type: 3,
+            presentNum: 3
+          }
+        ]
+      },
+      {
+        productNo: '002003',
+        productName: '中国银象棋12g',
+        price: 698.00,
+        PreferentialPolicy: [
+          {
+            type: 1,
+            discount: 0.9
+          },
+          {
+            type: 4,
+            minPrice: 3000,
+            cutDown: 350
+          },
+          {
+            type: 4,
+            minPrice: 2000,
+            cutDown: 30
+          },
+          {
+            type: 4,
+            minPrice: 1000,
+            cutDown: 10
+          }
+        ]
+      },
     ];
-    let orderCommandItems = orderCommandJSON.items;
+
+    let orderCommandItems = orderCommandJSON.items; // 用户订单
     let amount = 0; //合计总额
     let menuData = [];
-    for (let i= 0; i < orderCommandItems.length; i++) {
+    for (let i = 0; i < orderCommandItems.length; i++) {
       let sigleProduct = {};
-      for (let j= 0; j < items.length; j++) {
-        if ( orderCommandItems[i].product === items[j].productNo) {
+      for (let j = 0; j < items.length; j++) {
+        if (orderCommandItems[i].product === items[j].productNo) {
           sigleProduct = items[j];
           let sigleProductTotalPrice = items[j].price * orderCommandItems[i].amount;
           amount += sigleProductTotalPrice;
@@ -30,11 +132,64 @@ export default class OrderApp {
       }
       menuData.push(sigleProduct);
     }
-    console.log('-----------------------');
-    console.log(menuData);
-    console.log(amount);
-    // 2.客户可以使用账户余额购买贵金属，按客户等级计算积分，如果达到升级积分，则提升客户等级。
+
     // 3.客户支付时，可以使用打折券，对参与打折的商品享受折扣。
+      // 1. 获取用户使用的折扣券
+    let userDiscount = []; // 用户折扣券数组
+    orderCommandJSON.discountCards.forEach((item) => {
+      let value = '0.'+item.replace(/[^0-9]/g, '');
+      userDiscount.push(value);
+    });
+
+    // 计算优惠
+    let discountAmt = 0; // 优惠合计
+    for (let i = 0; i < menuData.length; i++) {
+      const singleProduct = menuData[i];
+      let PreferentialPolicy = singleProduct.PreferentialPolicy;
+      for (let j = 0; j < PreferentialPolicy.length; j++) {
+        const discountsItem = PreferentialPolicy[j];
+        switch (discountsItem.type) {
+          case 1:
+              computerDiscountAmt(discountsItem, singleProduct);
+            break;
+        
+          default:
+            break;
+        }
+      }
+      
+    }
+    // 折扣
+    function computerDiscountAmt (discountsItem, singleProduct) {
+      let canDiscount = userDiscount.filter(function(item) {
+        return (+item) === discountsItem.discount;
+      })[0];
+      canDiscount && (discountAmt += singleProduct.sigleProductTotalPrice - singleProduct.sigleProductTotalPrice * canDiscount);
+      // console.log('////////////////////////////////')
+      // console.log(discountAmt);
+      // console.log(singleProduct.sigleProductTotalPrice);
+    }
+    // 计算优惠信息
+    function computerOrderDiscounts (productItem) {
+
+    }  
+
+    // 2.客户可以使用账户余额购买贵金属，按客户等级计算积分，如果达到升级积分，则提升客户等级。
+    fs.readFile('test/resources/users.json', function (err, data) {
+      if (err) {
+        return err;
+      }
+      let usersData = JSON.parse(data.toString());
+      let loginUserData = usersData.filter(function (user) {
+        return user.memberId === orderCommandJSON.memberId;
+      })[0];
+      // computerRunkAndIntegral(loginUserData);
+    });
+
+    // 客户可以使用账户余额购买贵金属，按客户等级计算积分，如果达到升级积分，则提升客户等级。
+    // computerRunkAndIntegral(userData) {
+
+    // }
     // 4.根据促销规则，计算优惠金额。
     // 5.打印销售凭证。
     return (new OrderRepresentation({})).toString();
